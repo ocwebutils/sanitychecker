b97c3fe670badf24531155fe0bb87069da2902d8
ririxi <dev@ririxi.dev>
2023-06-11 22:17:40 +0200

    feat: add dialog polyfill

--- packages/client/src/components/form/formFooter.vue
+++ packages/client/src/components/form/formFooter.vue
@@ -4,3 +4,13 @@
 		<label class="btn" onclick="uploadedFilesModal.showModal()">Show Uploaded Results</label>
 	</div>
 </template>
+<script setup lang="ts">
+useHead({
+	script: [
+		{
+			src: "https://cdn.jsdelivr.net/gh/nuxodin/dialog-polyfill@v1.4.1/dialog.min.js",
+			type: "module"
+		}
+	]
+});
+</script>
